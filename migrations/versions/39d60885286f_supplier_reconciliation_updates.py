9ddf5751447260dc4e4a3ba3b13693596c69c3b9
farhathwin <95612554+farhathwin@users.noreply.github.com>
2025-06-30 22:21:36 +0530

    Fix migration down revision

---
+++ migrations/versions/39d60885286f_supplier_reconciliation_updates.py
@@ -0,0 +1,18 @@
+"""Add statement amount to reconciliation"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '39d60885286f'
+down_revision = '5317ad5f301d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('supplier_reconciliations', sa.Column('statement_amount', sa.Numeric(12, 2), nullable=True))
+
+
+def downgrade():
+    op.drop_column('supplier_reconciliations', 'statement_amount')
