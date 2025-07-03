a2b7f0eac05e6dfdce75262e3a0c9f0d5c7cd205
farhathwin <farhathwin@gmail.com>
2025-07-03 07:54:30 +0530

    latest change v5

--- mssqltest.py
+++ mssqltest.py
@@ -1,8 +1,8 @@
 import pymysql
 
 conn = pymysql.connect(
-    host="127.0.0.1",
-    user="root",
+    host="mleasd.mysql.pythonanywhere-services.com",
+    user="mleasd",
     password="@T@_$Ciy2kTSg_t"
 )
 print("Connected!")
