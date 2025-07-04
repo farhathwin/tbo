098c712b51de9e7fa45cc395e244ffcd68921224
farhathwin <farhathwin@gmail.com>
2025-07-04 19:24:16 +0530

    latest change v9.1

--- mssqltest.py
+++ mssqltest.py
@@ -2,9 +2,9 @@ import os
 import pymysql
 
 conn = pymysql.connect(
-    host="127.0.0.1",
-    user="root",
-    password="@T@_$Ciy2kTSg_t"
+    host="ls-c3344a1f3adca7ec35d9dee2e41c66d2f186212e.cttieryudfui.us-east-1.rds.amazonaws.com",
+    user="dbmasteruser",
+    password=")q(=KRREoC)cW5o5YaDgw$g6%(u6`9{b"
 )
 print("Connected!")
 conn.close()
